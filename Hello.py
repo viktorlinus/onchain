08daa8c4a5cdea7108ac7ee9b8d76badf6a1c459
viktorlinus <viktorlinus@hotmail.com>
2024-02-10 16:52:00 +0000

    adding nupl chart

--- Hello.py
+++ Hello.py
@@ -1,21 +1,52 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
 from streamlit.logger import get_logger
+import plotly.graph_objs as go
+from plotly.subplots import make_subplots
+import pandas as pd
 
 LOGGER = get_logger(__name__)
+
+# Function to create the plot using Plotly
+def create_chart(combined_df):
+    # Filter the DataFrame to only include data from 2012 onwards
+    combined_df_filtered = combined_df[combined_df.index.year >= 2012]
+
+    # Create a subplot with 2 y-axes
+    fig = make_subplots(specs=[[{"secondary_y": True}]])
+
+    # Add 'BTC Price' trace with a logarithmic scale
+    fig.add_trace(
+        go.Scatter(x=combined_df_filtered.index, y=combined_df_filtered['BTC Price'], name='BTC Price', mode='lines', line=dict(color='blue')),
+        secondary_y=False,
+    )
+
+    # Add 'Young-NUPL' trace
+    fig.add_trace(
+        go.Scatter(x=combined_df_filtered.index, y=combined_df_filtered['Young-NUPL'], name='Young-NUPL', mode='lines', line=dict(color='red')),
+        secondary_y=True,
+    )
+
+    # Add a horizontal line at 0 on the secondary y-axis
+    fig.add_hline(y=0, line=dict(color='green', dash='dash'), secondary_y=True)
+
+    # Set x-axis title
+    fig.update_xaxes(title_text='Date')
+
+    # Set y-axes titles
+    fig.update_yaxes(title_text='BTC Price (log scale)', secondary_y=False, type='log', tickcolor='blue')
+    fig.update_yaxes(title_text='Young-NUPL (linear scale)', secondary_y=True, tickcolor='red')
+
+    # Set the title
+    fig.update_layout(title='BTC Price and Young-NUPL Over Time')
+
+    return fig
+
+# Assuming the CSV file has columns 'Date', 'BTC Price', and 'Young-NUPL'
+# and that 'Date' is in a format that pandas can parse as a datetime object
+csv_file_path = 'nupl.csv'  # Replace with the path to your CSV file
+
+# Load the data from the CSV file
+combined_df = pd.read_csv(csv_file_path, index_col='Date', parse_dates=True)
 
 
 def run():
@@ -24,27 +55,11 @@ def run():
         page_icon="👋",
     )
 
-    st.write("# Welcome to Streamlit! 👋")
+    st.title("On-Chain Data")
 
-    st.sidebar.success("Select a demo above.")
-
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
+    # Plot the chart using the function defined above
+    fig = create_chart(combined_df)
+    st.plotly_chart(fig)
 
 
 if __name__ == "__main__":
